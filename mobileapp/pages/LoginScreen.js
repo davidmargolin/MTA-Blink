a2ee19a45cd593c29bf9b026be54aaf4ac32a555
Thita Charoentham <thita.charoentham35@qmail.cuny.edu>
2018-04-28 06:40:54 -0400

    login testing

--- mobileapp/pages/LoginScreen.js
+++ mobileapp/pages/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Text, View, Image, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import * as firebase from "firebase";
 // import Icon from 'react-native-vector-icons/FontAwesome';
 // import Container from '../components/Container';
@@ -26,18 +26,22 @@ export default class LoginScreen extends Component {
 
   render() {
     return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <View style = {styles.container}>
+              <Image resizeMode='contain' style= {{width: null, height:200,margin: 40}} source={require('../images/SEGA_logo.png')}/>
+              <Text style = {styles.title} >Hackathon MTA app</Text>
             <TextInput style = {styles.input}
                underlineColorAndroid = "transparent"
-               placeholder = "Email"
-               placeholderTextColor = "#605b6d"
+               placeholder = "Email:"
+               placeholderTextColor = 'rgba(255,255,255,0.7)'
                autoCapitalize = "none"
                onChangeText = {this.handleEmail}/>
 
             <TextInput style = {styles.input}
                underlineColorAndroid = "transparent"
-               placeholder = "Password"
-               placeholderTextColor = "#605b6d"
+               placeholder = "Password:"
+               placeholderTextColor = 'rgba(255,255,255,0.7)'
+               returnKeyType="go"
                autoCapitalize = "none"
                onChangeText = {this.handlePassword}/>
 
@@ -46,9 +50,10 @@ export default class LoginScreen extends Component {
                onPress = {
                   () => this.login(this.state.email, this.state.password)
                }>
-               <Text style = {styles.submitButtonText}> Submit </Text>
+               <Text style = {styles.submitButtonText}> LOGIN </Text>
             </TouchableOpacity>
          </View>
+         </KeyboardAvoidingView>
     );
   }
 }
@@ -57,21 +62,36 @@ export default class LoginScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-      paddingTop: 23
+      flex: 1,
+      paddingTop: 23,
+      backgroundColor: '#3498db'
+   },
+    logoContainer: {
+      paddingTop: 40,
+      height: 10,
+      width: 10
    },
    input: {
-      margin: 15,
+      padding: 20,
+      margin: 20,
       height: 40,
-      borderColor: '#7a42f4',
-      borderWidth: 1
+      borderColor: 'rgba(255,255,255,0.2)',
+      borderWidth: 1,
+      color: '#FFF'
    },
    submitButton: {
-      backgroundColor: '#7a42f4',
-      padding: 10,
-      margin: 15,
-      height: 40,
+      backgroundColor: '#2980b9',
+      margin: 20,
+      height: 50
    },
    submitButtonText:{
-      color: 'white'
-   }
+      color: 'white',
+      textAlign: 'center',
+      fontWeight: '700'
+
+   },
+   title: {
+    textAlign: 'center',
+    color:'white'
+    }
 });
