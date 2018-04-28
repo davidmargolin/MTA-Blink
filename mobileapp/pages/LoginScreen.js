078f03c33f70bf3b4a89704816dd722079f61455
Xin Yuan <sheanyxy@gmail.com>
2018-04-28 01:50:05 -0400

    adding login components

--- mobileapp/pages/LoginScreen.js
+++ mobileapp/pages/LoginScreen.js
@@ -1,27 +1,72 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import * as firebase from "firebase";
 
+
 export default class LoginScreen extends Component {
+      state = {
+        email: '',
+        password: ''
+     }
+     handleEmail = (text) => {
+        this.setState({ email: text })
+     }
+     handlePassword = (text) => {
+        this.setState({ password: text })
+     }
+     login = (email, pass) => {
+        alert('email: ' + email + ' password: ' + pass)
+     }
 
   componentDidMount() {
   }
 
   render() {
-
     return (
-      <View style={styles.container}>
-        <Text>Login Screen</Text>
-      </View>
+      <View style = {styles.container}>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Email"
+               placeholderTextColor = "#605b6d"
+               autoCapitalize = "none"
+               onChangeText = {this.handleEmail}/>
+
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Password"
+               placeholderTextColor = "#605b6d"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.login(this.state.email, this.state.password)
+               }>
+               <Text style = {styles.submitButtonText}> Submit </Text>
+            </TouchableOpacity>
+         </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      paddingTop: 23
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#7a42f4',
+      borderWidth: 1
+   },
+   submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+   },
+   submitButtonText:{
+      color: 'white'
+   }
 });
